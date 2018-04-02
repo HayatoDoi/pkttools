2dfd635f9fe99d765540ed3dcf1f23378563724c
Hayato Doi <b1517914@planet.kanazawa-it.ac.jp>
2018-04-02 10:25:38 +0900

    update

--- pkt-recv/main.go
+++ pkt-recv/main.go
@@ -5,10 +5,33 @@ import (
 	"github.com/HayatoDoi/pkttools/pkt"
 	"os"
 	"syscall"
+	"unsafe"
 )
+
+const (
+	SizeOfIfReq = 40
+	IFNAMSIZ    = 16
+)
+
+type ifReq struct {
+	Name  [IFNAMSIZ]byte
+	Flags uint16
+	pad   [SizeOfIfReq - IFNAMSIZ - 2]byte
+}
 
 func main() {
 	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, int(pkt.Htons(syscall.ETH_P_ALL)))
+	// _, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, int(pkt.Htons(syscall.ETH_P_ALL)))
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	var ifr ifReq
+	fmt.Println(ifr)
+	copy(ifr.Name[:15], "br-123293634eeb")
+	fmt.Println(ifr)
+	err = pkt.Ioctl(uintptr(fd), uintptr(syscall.SIOCGIFINDEX), uintptr(unsafe.Pointer(&ifr)))
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
@@ -22,6 +45,6 @@ func main() {
 			fmt.Println(err)
 			panic(err)
 		}
-		fmt.Printf("% X\n", buf[:numRead])
+		fmt.Printf("%X\n", buf[:numRead])
 	}
 }
